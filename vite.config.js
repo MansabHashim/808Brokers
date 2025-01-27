d59a7e1aa75e911b9efc54bdd57857048a1d9cba
partiallycloudy <hbm.wallet@gmail.com>
2025-01-27 10:52:41 -0600

    Update domain to 808brokers.cc

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 5173,
     open: true,
@@ -14,7 +15,6 @@ export default defineConfig({
       },
     }
   },
-  base: '/',
   optimizeDeps: {
     include: ['framer-motion']
   }
